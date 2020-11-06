f06f1d3ca43cb19ab0e5195bde1c560a5c5ef282
Mason  Lewis <mlewis24@biomedscienceacademy.org>
2020-11-06 15:58:05 +0000

    Madlibs project is partly finished. Still has a syntax error that I will fix in next commit & push.

--- main.py
+++ main.py
@@ -1,7 +1,25 @@
 """
 MadLibs
-Author: 
-Period/Core:
+Author: Mason Lewis
+Period/Core: Core 6
 
 
 """
+
+name = input("Enter a name: ")
+color = input("Enter an color: ")
+adverb = input("Enter an adverb: ")
+food1 = input("Enter a food: ")
+food2 = input("Enter another food: ")
+noun = input("Enter a noun: ")
+place = input("Enter a place: ")
+verb = input("Enter a verb: ")
+skill = input("Enter one skill you have: ")
+
+print("" + name + " was thinking of a place to travel to. Their mother said why not " + place + "?"
+
+print("" + name + " thought that idea was really amazing! Her mom gave her breakfast that consisted of " + food1 + " and " + food2 + "."
+
+print("" + name + " will have to practice her " + skill + " " + adverb + " to make it harder to " + verb + " with humans."
+
+print("" + name + " has a short journey of places to see, including the " + noun + " house and the " + color + " sky."
